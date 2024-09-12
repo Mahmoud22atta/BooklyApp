8983ce8e7b722061ba84f4688ab49b8f2532d289
YourUsername <you@example.com>
2024-09-12 17:13:41 +0300

    first commit

--- lib/Featuers/splash/presentaition/views/widgets/splash_view_body.dart
+++ lib/Featuers/splash/presentaition/views/widgets/splash_view_body.dart
@@ -1,8 +1,27 @@
+import 'package:booklyapp/Featuers/splash/presentaition/views/widgets/sliding_text.dart';
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> animation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    animation = Tween<Offset>(begin: Offset(0, 10), end: Offset(0, 0))
+        .animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +30,7 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Center(child: Image.asset(AssetsDats.logo)),
+        SlidingText(animation: animation)
       ],
     );
   }
